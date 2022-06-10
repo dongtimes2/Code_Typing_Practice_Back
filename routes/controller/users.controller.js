f98a49602a5bfe0a78123542ce28e05d42a35b6f
dongtimes2 <dongtimes2@gmail.com>
2022-06-10 20:36:30 +0900

    feat: 유저의 타자 결과를 patch 요청을 통해 서버에 저장시키고, 또 get 요청을 통해 유저의 지난 결과 값을 불러와 전달해줄 수 있는 로직 구현

--- routes/controller/users.controller.js
+++ routes/controller/users.controller.js
@@ -46,8 +46,76 @@ exports.patch = async (req, res, next) => {
   res.json('users_ok');
 };
 
-exports.recordGet = (req, res, next) => {
-  console.log(req.params.id);
-  console.log(req.params.language);
-  res.json('record_ok');
+exports.recordGet = async (req, res, next) => {
+  const id = req.params.id;
+  const language = req.params.language;
+
+  try {
+    const userInfomation = await User.findById(id).lean();
+
+    if (!userInfomation) {
+      return next({ status: 400, message: 'Bad Request' });
+    }
+
+    const totalRecords = userInfomation.languageRecord;
+    const result = [];
+
+    for (const record of totalRecords) {
+      if (record.language === language) {
+        const data = {
+          typingSpeed: record.typingSpeed,
+          accuracy: record.accuracy,
+          time: record.time,
+        };
+
+        result.push(data);
+      }
+    }
+
+    res.json(result);
+  } catch (err) {
+    return next(err);
+  }
+};
+
+exports.recordPatch = async (req, res, next) => {
+  const id = req.params.id;
+  const language = req.params.language;
+  const { typingSpeed, accuracy, time } = req.body;
+
+  if (!LANGUAGE_LIST.includes(language)) {
+    return next({ status: 400, message: 'Invalid Programming Language' });
+  }
+
+  if (typeof typingSpeed !== 'number') {
+    return next({ status: 400, message: 'typingSpeed is not a number type' });
+  }
+
+  if (typeof accuracy !== 'number') {
+    return next({ status: 400, message: 'accuracy is not a number type' });
+  }
+
+  try {
+    const result = await User.findOneAndUpdate(
+      { _id: id },
+      {
+        $push: {
+          languageRecord: {
+            language,
+            typingSpeed,
+            accuracy,
+            time,
+          },
+        },
+      },
+      { new: true },
+    );
+
+    if (!result) {
+      return next({ status: 400, message: 'Bad Request' });
+    }
+  } catch (err) {
+    return next(err);
+  }
+  res.json('record_patch_ok');
 };
