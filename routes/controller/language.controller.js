6a04e58330188409433a150a353ebc92c87beb73
dongtimes2 <dongtimes2@gmail.com>
2022-06-12 00:03:54 +0900

    feat: 타입별 문제 정보를 불러올 때, 쿼리로 들어오는 타입이 올바른지 확인하는 에러핸들링 추가 구현

--- routes/controller/language.controller.js
+++ routes/controller/language.controller.js
@@ -2,12 +2,18 @@ const { Paragraph } = require('../../models/Paragraph');
 const { Sentence } = require('../../models/Sentence');
 const { Word } = require('../../models/Word');
 const LANGUAGE_LIST = require('../../utils/constants').LANGUAGE_LIST;
+const PRACTICE_TYPE = require('../../utils/constants').PRACTICE_TYPE;
 
 exports.get = async (req, res, next) => {
   const practiceType = req.query.type;
+  const language = req.params.language;
 
-  if (!LANGUAGE_LIST.includes(req.params.language)) {
+  if (!LANGUAGE_LIST.includes(language)) {
     return next({ status: 400, message: 'Invalid Programming Language' });
+  }
+
+  if (!PRACTICE_TYPE.includes(practiceType)) {
+    return next({ status: 400, message: 'Invalid practice type' });
   }
 
   try {
