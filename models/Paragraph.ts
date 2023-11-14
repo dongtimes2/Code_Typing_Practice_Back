409d24265da038f2fadaac03362d61790382e111
dongtimes2 <dongtimes2@gmail.com>
2023-11-14 20:52:07 +0900

    file: 사용하지 않는 레거시 모델 제거

--- models/Paragraph.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const paragraphSchema = new mongoose.Schema({
-  language: String,
-  content: String,
-});
-
-export const Paragraph = mongoose.model('Paragraph', paragraphSchema);
--- models/Sentence.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const sentenceSchema = new mongoose.Schema({
-  language: String,
-  content: String,
-});
-
-export const Sentence = mongoose.model('Sentence', sentenceSchema);
