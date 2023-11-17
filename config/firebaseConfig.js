b2988c8012abb3358bb61e1116d6b47c32490729
dongtimes2 <dongtimes2@gmail.com>
2023-11-17 19:31:48 +0900

    file: 레거시 설정파일 제거

--- config/firebaseConfig.js
+++
@@ -1,6 +0,0 @@
-const admin = require('firebase-admin');
-const serviceSecret = JSON.parse(process.env.SERVICE_SECRET_KEY);
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceSecret),
-});
