3a237b54691c3031ea8108365b2bcc4968b6e7ca
dongtimes2 <dongtimes2@gmail.com>
2023-11-17 19:48:49 +0900

    feat: 레거시 firebase 로더 제거

--- loaders/firebase.ts
+++
@@ -1,7 +0,0 @@
-import admin from 'firebase-admin';
-
-import { SERVICE_SECRET_KEY } from '../config/env';
-
-admin.initializeApp({
-  credential: admin.credential.cert(JSON.parse(SERVICE_SECRET_KEY)),
-});
