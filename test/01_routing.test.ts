7a7f96714f709cf8c846b75f15cc598e6374ce71
dongtimes2 <dongtimes2@gmail.com>
2024-01-08 03:30:44 +0900

    feat: 404 응답에 대한 테스트 코드 추가

--- test/01_routing.test.ts
+++ test/01_routing.test.ts
@@ -6,4 +6,8 @@ describe('기본 연결 테스트', () => {
   test('서버와 연결되어야 합니다', (done) => {
     request(app).get('/').expect(200, '"server_status_ok"', done);
   });
+
+  test('잘못된 경로로 요청하면 404를 응답해야 합니다', (done) => {
+    request(app).get('/wrong').expect(404, done);
+  });
 });
